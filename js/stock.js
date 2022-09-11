56ba430782e3fa3014a7e0a3ec050152c8097dd8
zowyfps <1rociozoe@gmail.com>
2022-09-10 21:15:29 -0300

    stock update

---
+++ js/stock.js
@@ -0,0 +1,19 @@
+let stockProductos = [
+    {id: 1, freeshipping: false, nombre: "DAX J", tipo: "EVENTO", desc: "9/7/2022 OTC", precio: 2000,  img: './assets/images/dax.png'},
+    {id: 2, freeshipping: true, nombre: "I HATE MODELS", tipo: "EVENTO", desc: "16/7/2022 OTC", precio: 3000,  img: './assets/images/ihate.png'},
+    {id: 3, freeshipping: false, nombre: "MARIANO DC", tipo: "EVENTO", desc: "23/7/2022 OTC", precio: 500, img: './assets/images/mariano.png'},
+    {id: 4, freeshipping: false, nombre: "Dr RUBENSTEIN", tipo: "EVENTO", desc: "30/7/2022 OTC", precio: 500,  img: './assets/images/rubi.png'},
+    {id: 5, freeshipping: false, nombre: "Michel Laureola", tipo: "EVENTO", desc: "9/7/2022 OTC", precio: 300,  img: './assets/images/michellaureola.png'},
+    {id: 6, freeshipping: false, nombre: "FABIO FLORIDO", tipo: "EVENTO", desc: "17/9/2022OTC", precio: 1500,  img: './assets/images/fabioflorido.png'},
+    {id: 7, freeshipping: false, nombre: "DANZA NATIVA", tipo: "EVENTO", desc: "24/9/2022 OTC", precio: 2000,  img: './assets/images/danzanativa.png'},
+    {id: 8, freeshipping: false, nombre: "CLERIC", tipo: "EVENTO", desc: "6/11/2022 OTC", precio: 3000,  img: './assets/images/cleric.png'},
+    {id: 9, freeshipping: false, nombre: "DINAMITE 7 HORAS TRESOR NIGHT", tipo: "EVENTO", desc: "9/7/2022 OTC", precio: 5000,  img: './assets/images/dinamite.png'},
+    {id: 10, freeshipping: false, nombre: "100 COMBOS NIGHT", tipo: "EVENTO", desc: "9/7/2022 OTC", precio: 1000,  img: './assets/images/100combos.png'},
+    {id: 11, freeshipping: false, nombre: "VOLTAGE CONTROLLED", tipo: "EVENTO", desc: "4/11/2022", precio: 1000,  img: './assets/images/voltagecontrolled.png'},
+    {id: 12, freeshipping: false, nombre: "UNDER CLUB SHOWCASE NEUQUÉN", tipo: "EVENTO", desc: "6/10/2022 OTC", precio: 2000,  img: './assets/images/ucshowcase.png'},
+    {id: 13, freeshipping: false, nombre: "MARIANO DC ", tipo: "EVENTO", desc: "14/10/2022 OTC", precio: 300,  img: './assets/images/marianodc.png'},
+    {id: 14, freeshipping: false, nombre: "+ UNDER CLUB DIA UNO", tipo: "EVENTO", desc: " 7/12/2022 OTC", precio: 700,  img: './assets/images/dia1sietediciembre.png'},
+    {id: 15, freeshipping: false, nombre: "+ UNDER CLUB DIA DOS", tipo: "EVENTO", desc: "9/7/2022 OTC", precio: 800,  img: './assets/images/dia2ochodiciembre.png'},
+    {id: 16, freeshipping: false, nombre: "+ UNDER CLUB COMBO", tipo: "EVENTO", desc: "7/12/2022 OTC", precio: 500,  img: './assets/images/78diciembre.png'},
+
+]
